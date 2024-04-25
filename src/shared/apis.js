b0d235bfb92b1726647d1f69853d0d715a631913
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2024-04-25 21:23:06 +0200

    Updated api urls

--- src/shared/apis.js
+++ src/shared/apis.js
@@ -2,3 +2,6 @@ const API_BASE_URL = "https://v2.api.noroff.dev";
 export const API_VENUES = `${API_BASE_URL}/holidaze/venues`;
 export const API_BOOKINGS = `${API_BASE_URL}/holidaze/bookings`;
 export const API_PROFILES = `${API_BASE_URL}/holidaze/profiles`;
+export const API_REGISTER_URL = `${API_BASE_URL}/auth/register`;
+export const API_LOGIN_URL = `${API_BASE_URL}/auth/login`;
+export const API_KEY_URL = `${API_BASE_URL}/auth/create-api-key`;
