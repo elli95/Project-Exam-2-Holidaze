7e6591fee81646f52178c190a51761d679d7cdde
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2024-05-26 18:30:28 +0200

    Fixed img files

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,12 +44,8 @@ module.exports = {
       xxl: "1780px",
     },
     colors: {
-      transparent: "transparent",
-      current: "currentColor",
       white: "#ffffff",
       black: "#000000",
-      purple: "#3f3cbb",
-      midnight: "#121063",
       metal: "#565584",
       tahiti: "#3ab7bf",
       silver: "#ecebff",
