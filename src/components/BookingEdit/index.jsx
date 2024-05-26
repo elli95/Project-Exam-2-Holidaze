c385e74fe3ba80bd5c3881b2605e108e37cf434b
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2024-05-26 14:46:33 +0200

    doc comments and css fix

--- src/components/BookingEdit/index.jsx
+++ src/components/BookingEdit/index.jsx
@@ -6,9 +6,22 @@ import { API_BOOKINGS } from "../../shared/apis";
 import useApiCall from "../../hooks/useApiCall";
 import useVenues from "../../store/venueLocations";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWifi, faSquareParking, faMugHot, faPaw } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faWifi, faSquareParking, faMugHot, faPaw } from "@fortawesome/free-solid-svg-icons";
 
-// function BookingEdit({ setVenueBookingData = () => {}, fetchVenueBookingData = () => {}, venueId }) {
+/**
+ * BookingEdit component allows users to edit their booking details.
+ * @component
+ * @param {function} setVenueIdToShow - Function to set the ID of the venue to show.
+ * @param {function} setBooking - Function to set the booking details.
+ * @param {string} venueId - The ID of the venue.
+ * @param {function} handleCloseBtn - Function to handle closing the modal.
+ * @param {function} onDeleteBooking - Function to delete a booking.
+ * @returns {JSX.Element} - JSX element representing the BookingEdit component.
+ * @example
+ * // Example usage of BookingEdit component:
+ * import BookingEdit from './BookingEdit';
+ * <BookingEdit setVenueIdToShow={setVenueIdToShow} setBooking={setBooking} venueId={venueId} handleCloseBtn={handleCloseBtn} onDeleteBooking={onDeleteBooking} />
+ */
 function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, onDeleteBooking }) {
   const { validateField } = useVenues();
   const { apiKey } = usePostApiKey();
@@ -36,6 +49,11 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     guests: "",
   });
 
+  /**
+   * Handles the onBlur event for input fields.
+   * Performs validation based on the input field's name and updates errors state accordingly.
+   * @param {Event} event - The onBlur event object.
+   */
   const handleBlur = (event) => {
     const { name, value } = event.target;
     const newErrors = { ...errors };
@@ -70,6 +88,10 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     editVenueFilter = profileData.bookings.filter((venue) => venue.id === venueId);
   }
 
+  /**
+   * Calculates and updates the result based on the start date, end date, and venue price.
+   * Runs whenever there's a change in the start date, end date, or venue filter.
+   */
   useEffect(() => {
     if (editVenueFilter) {
       const oneDay = 24 * 60 * 60 * 1000;
@@ -80,6 +102,11 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     }
   }, [startDate, endDate, editVenueFilter]);
 
+  /**
+   * Formats a given date string to YYYY-MM-DD format.
+   * @param {string} dateString - The date string to format.
+   * @returns {string} The formatted date string in YYYY-MM-DD format.
+   */
   const formatDate = (dateString) => {
     if (!dateString) return "";
     const date = new Date(dateString);
@@ -97,6 +124,14 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
   }
   const runCount = useRef(0);
 
+  /**
+   * useEffect hook that runs only once when the component mounts and if editVenueFilter exists.
+   * It sets the start and end dates based on the initial values from editVenueFilter.
+   * @param {function} handleStartDateChange - Function to handle start date change.
+   * @param {function} handleEndDateChange - Function to handle end date change.
+   * @param {object} editVenueFilter - Object containing venue filter data.
+   * @param {object} runCount - Ref object to keep track of how many times the effect has run.
+   */
   useEffect(() => {
     if (runCount.current === 0 && editVenueFilter) {
       handleStartDateChange({ target: { value: editVenueFilter[0].dateFrom } });
@@ -109,10 +144,15 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     return <div className="loading"></div>;
   }
 
+  /**
+   * Component representing a confirmation modal for editing or canceling a booking.
+   * @param {function} onConfirm - Function to handle confirmation action.
+   * @param {function} onCancel - Function to handle cancellation action.
+   */
   const ConfirmationModal = ({ onConfirm, onCancel }) => {
     const message = actionType === "submit" ? "Are you sure you want to edit this booking?" : "Are you sure you want to cancel this booking?";
     return (
-      <div className="overlayCheck" onTouchStart={(e) => e.stopPropagation()}>
+      <div className="overlayCheck bookingEditModuleHight" onTouchStart={(e) => e.stopPropagation()}>
         <div className="modulePosition flex flex-col justify-center rounded-lg">
           <p className="text-xl text-center">{message}</p>
           <div className="flex gap-5 justify-evenly pt-5">
@@ -128,6 +168,10 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     );
   };
 
+  /**
+   * Handles form submission for editing a booking.
+   * @param {object} event - The form submission event.
+   */
   const handleSubmit = (event) => {
     event.preventDefault();
     const updatedFormState = {
@@ -149,11 +193,11 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     console.log("handleConfirm:", confirmHandler);
   };
 
-  // console.log("handleConfirm2:", confirmHandler);
-  // console.log("formState out:", formState);
-
+  /**
+   * Handles confirmation of editing or deleting a booking.
+   * @param {object} updatedFormState - The updated form state containing booking details.
+   */
   const handleConfirm = async (updatedFormState) => {
-    // console.log("formState:", updatedFormState);
     setShowModal(false);
     try {
       const updatedProfileData = await apiCall(
@@ -172,9 +216,6 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
           prevVenues.map((venue) => (venue.id === updatedProfileData.data.id ? { ...venue, ...updatedProfileData.data } : venue))
         );
         handleCloseBtn();
-        // setVenueIdToShow(null);
-        // setIsVenueBookingsShown(false);
-        // setIsBookingsShown(false);
       } else {
         console.log("Error:", updatedProfileData);
         console.log("Error:", updatedProfileData.errors[0].message);
@@ -185,13 +226,21 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     }
   };
 
+  /**
+   * Handles the initiation of the booking deletion process.
+   */
   const handleDelete = async () => {
-    console.log("hello :D");
     setShowModal(true);
     setActionType("delete");
     setConfirmHandler(() => handleDeleteConfirm);
   };
 
+  /**
+   * Handles the confirmation of booking deletion.
+   * Initiates the deletion of the booking from the backend.
+   * If successful, updates the state to remove the booking and resets the venue to show.
+   * If unsuccessful, sets an error message.
+   */
   const handleDeleteConfirm = async () => {
     setShowModal(false);
 
@@ -205,8 +254,6 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
       if (!updatedProfileData) {
         onDeleteBooking(venueId);
         setVenueIdToShow(null);
-        // setIsCreateBookingShown(false);
-        // fetchBookingData();
       } else {
         console.log("Error:", updatedProfileData.errors[0].message);
         setErrorMessage("There was an error: " + updatedProfileData.errors[0].message);
@@ -216,6 +263,9 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
     }
   };
 
+  /**
+   * Cancels the current action and closes the confirmation modal.
+   */
   const handleCancel = () => {
     setShowModal(false);
   };
@@ -236,30 +286,39 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
         ) : (
           <div>
             <div>
-              <div className="imgBox">
-                {editVenueFilter[0].venue.media[0] && <img src={editVenueFilter[0].venue.media[0].url} alt={editVenueFilter[0].venue.media[0].alt} />}
+              <div className="imgBox pt-2.5">
+                {editVenueFilter[0].venue.media[0] ? (
+                  <img src={editVenueFilter[0].venue.media[0].url} alt={editVenueFilter[0].venue.media[0].alt} />
+                ) : (
+                  <img src="https://images.unsplash.com/photo-1579547945413-497e1b99dac0" alt="Venue" />
+                )}
               </div>
               <div className="p-3">
-                <div className="flex justify-between">
-                  <h2>{editVenueFilter[0].venue.name}</h2>
-                  <p>⭐{editVenueFilter[0].venue.rating}</p>
+                <div className="flex justify-between text-xl">
+                  <h2>{editVenueFilter[0].venue.name || "Venue Name Not Available"}</h2>
+                  <div className="flex gap-1 text-xl">
+                    <p className="text-star">
+                      <FontAwesomeIcon icon={faStar} />
+                    </p>
+                    <p>{editVenueFilter[0].venue.rating}</p>
+                  </div>
                 </div>
-                <div className="flex flex-col justify-center gap-2.5 items-center">
+                <div className="flex flex-col justify-center gap-2.5 items-center text-lg">
                   <div className="flex flex-col w-64">
                     <div className="flex justify-between bg-greyBlur px-1">
                       <h3>Country:</h3>
-                      <h3>{editVenueFilter[0].venue.location.country}</h3>
+                      <h3>{editVenueFilter[0].venue.location.country || "Country Name Not Available"}</h3>
                     </div>
                     <div className="flex justify-between px-1">
                       <h3>City:</h3>
-                      <h3>{editVenueFilter[0].venue.location.city}</h3>
+                      <h3>{editVenueFilter[0].venue.location.city || "City Not Available"}</h3>
                     </div>
                     <div className="flex justify-between bg-greyBlur px-1">
                       <h3>Address:</h3>
-                      <h3>{editVenueFilter[0].venue.location.address}</h3>
+                      <h3>{editVenueFilter[0].venue.location.address || "Address Not Available"}</h3>
                     </div>
                   </div>
-                  <div>
+                  <div className="flex flex-col gap-2.5">
                     <div className="flex flex-col w-64">
                       <div className="flex justify-between px-1">
                         <h3>Guests:</h3>
@@ -270,37 +329,37 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
                         <h3>{editVenueFilter[0].venue.price}</h3>
                       </div>
                     </div>
-                    <div className="flex justify-between w-64 gap-2 py-2">
-                      <h3
-                        title={`Wifi${editVenueFilter[0].venue.meta.wifi ? "" : " not"} included`}
-                        className={`${editVenueFilter[0].venue.meta.wifi ? "metaIconTrue" : "metaIconFalse"}`}
-                      >
-                        <FontAwesomeIcon icon={faWifi} />
-                      </h3>
-                      <h3
-                        title={`Parking${editVenueFilter[0].venue.meta.parking ? "" : " not"} available`}
-                        className={`${editVenueFilter[0].venue.meta.parking ? "metaIconTrue" : "metaIconFalse"}`}
-                      >
-                        <FontAwesomeIcon icon={faSquareParking} />
-                      </h3>
-                      <h3
-                        title={`Breakfast${editVenueFilter[0].venue.meta.breakfast ? "" : " not"} included`}
-                        className={`${editVenueFilter[0].venue.meta.breakfast ? "metaIconTrue" : "metaIconFalse"}`}
-                      >
-                        <FontAwesomeIcon icon={faMugHot} />
-                      </h3>
-                      <h3
-                        title={` ${editVenueFilter[0].venue.meta.pets ? "Pets" : "No pets"} permitted`}
-                        className={`${editVenueFilter[0].venue.meta.pets ? "metaIconTrue" : "metaIconFalse"}`}
-                      >
-                        <FontAwesomeIcon icon={faPaw} />
-                      </h3>
+                    <div className="flex justify-center h-10 gap-2.5">
+                      {editVenueFilter[0].venue.meta.wifi && (
+                        <div title="Wifi included" className="metaIconTrue">
+                          <FontAwesomeIcon icon={faWifi} />
+                          <span className="sr-only">Wifi included</span>
+                        </div>
+                      )}
+                      {editVenueFilter[0].venue.meta.parking && (
+                        <div title="Parking available" className="metaIconTrue">
+                          <FontAwesomeIcon icon={faSquareParking} />
+                          <span className="sr-only">Parking available</span>
+                        </div>
+                      )}
+                      {editVenueFilter[0].venue.meta.breakfast && (
+                        <div title="Breakfast included" className="metaIconTrue">
+                          <FontAwesomeIcon icon={faMugHot} />
+                          <span className="sr-only">Breakfast included</span>
+                        </div>
+                      )}
+                      {editVenueFilter[0].venue.meta.pets && (
+                        <div title="Pets permitted" className="metaIconTrue">
+                          <FontAwesomeIcon icon={faPaw} />
+                          <span className="sr-only">Pets permitted</span>
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="text-lg">
               <div className="flex gap-12 justify-center">
                 <div className="flex flex-col items-center">
                   <label>Start Date:</label>
@@ -336,16 +395,6 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
                     </option>
                   ))}
                 </select>
-                {/* <input
-                  type="number"
-                  name="guests"
-                  min="1"
-                  max={editVenueFilter[0].venue.maxGuests}
-                  pattern="[0-9]*"
-                  defaultValue={editVenueFilter[0].guests}
-                  onBlur={handleBlur}
-                  className="bg-greyBlur w-20 pl-1"
-                ></input> */}
                 <span className="error">{errors.guests}</span>
               </div>
               <p className="flex justify-center">Total: {result}</p>
@@ -360,8 +409,6 @@ function BookingEdit({ setVenueIdToShow, setBooking, venueId, handleCloseBtn, on
                 Cancel Booking
               </button>
             </div>
-            {/* {showModal && <ConfirmationModal onConfirm={handleDeleteConfirm} onCancel={handleCancel} />} */}
-            {/* {showModal && <ConfirmationModal onConfirm={handleConfirm} onCancel={handleCancel} />} */}
             {showModal && <ConfirmationModal onConfirm={confirmHandler} onCancel={handleCancel} />}
 
             {errorMessage && <span className="error flex justify-center pt-2.5 text-xl">{errorMessage}</span>}
--- src/pages/profilePage/index.js
+++ src/pages/profilePage/index.js
@@ -1,8 +1,16 @@
 import ProfileInfo from "../../components/ProfileInfo";
 
+/**
+ * ProfilePage component displays the user's profile information.
+ * @component
+ * @example
+ * // Example usage of ProfilePage component:
+ * import ProfilePage from './ProfilePage';
+ * <ProfilePage />
+ */
 function ProfilePage() {
   return (
-    <div className="mainDiv">
+    <div className="mainDiv py-8 mt-10 lg:pt-0 lg:mt-0">
       <ProfileInfo />
     </div>
   );
